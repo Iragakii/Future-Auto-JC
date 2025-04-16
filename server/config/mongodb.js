4715ae8466d7249ffd020eeb158c4cd790fdf0ef
iragaki <jcgroup.vni@gmail.com>
2025-04-16 12:14:20 +0700

    one more step

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("MongoDB connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/carDealership`);
 };
 
 export default connectDB;
