8b20b0159bd75093ec1f27a3cb7de678fb8b074e
iragaki <jcgroup.vni@gmail.com>
2025-04-19 17:26:00 +0700

    done

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("MongoDB connected"));
-  await mongoose.connect("mongodb://localhost:27017/carDealership"); // Hardcoded connection string for testing
+  const mongoUri =
+    process.env.MONGODB_URI || "mongodb://localhost:27017/carDealership";
+  await mongoose.connect(mongoUri);
 };
 
 export default connectDB;
